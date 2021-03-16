4748ece4206a349f76669f141fb19d07a4143fc1
InK <InKryption07@gmail.com>
2021-03-16 22:50:25 +0100

    Got some good stuff here

--- Grid.hpp
+++ Grid.hpp
@@ -16,72 +16,216 @@ namespace ink {
 		
 		
 		// Number of elements in the Grid
-		public: static constexpr size_t size()
+		public: static constexpr size_t
+		size()
 		{ return SIZE; }
 		
 		// Number of elements in a column
-		public: static constexpr size_t size_col()
+		public: static constexpr size_t
+		size_col()
 		{ return COLS; }
 		
 		// Number of elements in a row
-		public: static constexpr size_t size_row()
+		public: static constexpr size_t
+		size_row()
 		{ return ROWS; }
 		
 		
 		
 		// Given a column and row index, returns the equivalent theoretical absolute index as in the underlying 1D array.
-		public: static constexpr size_t Index_Absolute(size_t col, size_t row)
+		public: static constexpr size_t
+		IndexAbs(size_t col, size_t row)
 		{ return row * COLS + col; }
 		
 		// Given an absolute index, returns the equivalent theoretical column index.
-		public: static constexpr size_t Index_Column(size_t abs)
+		public: static constexpr size_t
+		IndexCol(size_t abs)
 		{ return abs % COLS; }
 		
 		// Given an absolute index, returns the equivalent theoretical row index.
-		public: static constexpr size_t Index_Row(size_t abs)
-		{ return (abs - Index_Column(abs)) / COLS; }
+		public: static constexpr size_t
+		IndexRow(size_t abs)
+		{ return (abs - IndexCol(abs)) / COLS; }
 		
 		
 		
 		// Default constructor
-		public: constexpr Grid() requires (std::is_default_constructible_v<T>):
-		M_data({}) {}
+		public: constexpr
+		Grid() requires (std::is_default_constructible_v<T>):
+		M_data() {}
 		
-		public: constexpr Grid(Grid const& other)
+		// Copy constructor
+		public: constexpr
+		Grid(Grid const& other)
 		{ std::copy(other.M_data, other.M_data + SIZE, M_data); }
 		
-		public: constexpr Grid(Grid && other)
+		// Move constructor
+		public: constexpr
+		Grid(Grid && other)
 		{ std::copy(other.M_data, other.M_data + SIZE, M_data); }
 		
 		// Construct all elements using given arguments
-		public: template<typename... ctr> requires( std::constructible_from<T, ctr...> ) explicit constexpr Grid(ctr&&... args)
+		public: template<typename... ctr> requires( std::constructible_from<T, ctr...> ) explicit constexpr
+		Grid(ctr&&... args)
 		{ for (size_t i = 0; i < SIZE; i++) M_data[i] = T(std::forward<ctr>(args)...); }
 		
 		// List constructor. Every N contiguous elements will be part of a row, where N is the number of columns.
-		public: constexpr Grid(std::initializer_list<T> list)
+		public: constexpr
+		Grid(std::initializer_list<T> list)
 		{ std::copy(list.begin(), list.begin() + list.size(), M_data); }
 		
-		// 2D List constructor. Every inner list is a row.
-		public: constexpr Grid(std::initializer_list<std::initializer_list<T>> list)
-		{  }
-		
 		// Span constructor. Every N contiguous element will be part of a row, where N is the number of columns.
-		public: constexpr Grid(std::span<T, SIZE> const& span)
+		public: constexpr
+		Grid(std::span<T, SIZE> const& span)
 		{ std::copy(span.begin(), span.end(), M_data); }
 		
 		// 2D Array Constructor. Works as expected.
-		public: constexpr Grid(const T(&array2D)[COLS][ROWS]) {
+		public: constexpr
+		Grid(const T(&array2D)[COLS][ROWS]) {
 			T const* arr2d = static_cast<int const*>(static_cast<void const*>(array2D));
 			std::copy(arr2d, arr2d + SIZE, M_data);
 		}
 		
+		private: template<typename _t, bool isCol>
+		struct Slice {
+			
+			public: constexpr auto&
+			operator[](size_t i)
+			{
+				if constexpr(isCol)		return M_grid[{M_section, i}];
+				if constexpr(!isCol)	return M_grid[{i, M_section}];
+			}
+			
+			private: constexpr
+			Slice(Slice const&) = default;
+			
+			private: constexpr
+			Slice(Slice &&) = default;
+			
+			private: constexpr
+			Slice(_t& grid, size_t i):
+			M_grid(grid), M_section(i) {}
+			
+			
+			struct Iterator {
+				using value_type = std::remove_reference_t<decltype(Slice(std::declval<Grid&>(),0).operator[](0))>;
+				using reference = value_type&;
+				using pointer = value_type*;
+				using difference_type = std::ptrdiff_t;
+				using iterator_category = std::bidirectional_iterator_tag;
+				
+				public: constexpr bool
+				operator!=(Iterator const& other) const
+				{ return M_curr != other.M_curr; }
+				
+				public: constexpr bool
+				operator==(Iterator const& other) const
+				{ return M_curr == other.M_curr; }
+				
+				public: constexpr reference 
+				operator*()
+				{ return (*M_slice)[M_curr]; }
+				
+				public: constexpr pointer
+				operator->()
+				{ return &((*M_slice)[M_curr]); }
+				
+				public: constexpr Iterator
+				operator+(difference_type i)
+				{ return Iterator(M_slice, M_curr + i); }
+				
+				public: constexpr Iterator
+				operator-(difference_type i)
+				{ return (*this) + (-i); }
+				
+				public: constexpr Iterator
+				operator++(int)
+				{ return Iterator(M_slice, M_curr++); }
+				
+				public: constexpr Iterator&
+				operator++()
+				{ M_curr++; return *this; }
+				
+				public: constexpr Iterator
+				operator--(int)
+				{ return Iterator(M_slice, M_curr--); }
+				
+				public: constexpr Iterator&
+				operator--()
+				{ M_curr--; return *this; }
+				
+				public: constexpr Iterator&
+				operator+=(difference_type i)
+				{ M_curr += i; return *this; }
+				
+				public: constexpr Iterator&
+				operator-=(difference_type i)
+				{ M_curr -= i; return *this; }
+				
+				private: friend
+				class Slice;
+				
+				private: Iterator(Slice* slice, size_t i):
+				M_slice(slice), M_curr(i) {}
+				
+				private: Slice*
+				M_slice{nullptr};
+				
+				private: size_t
+				M_curr = 0;
+				
+			};
+			
+			public: constexpr auto
+			begin()
+			{ return Iterator(this, 0); }
+			
+			public: constexpr auto
+			end()
+			{ return Iterator(this, (isCol) ? COLS : ROWS ); }
+			
+			private: friend
+			class Grid;
+			
+			private: _t&
+			M_grid;
+			
+			private: size_t
+			M_section = 0;
+			
+		};
 		
+		public: constexpr auto
+		GetCol(size_t i)
+		{ return Slice<std::remove_reference_t<decltype(*this)>, true>(*this, i); }
 		
-		public: constexpr auto& operator[](std::initializer_list<size_t> col_row)
-		{ return M_data[Index_Absolute(col_row.begin()[0],col_row.begin()[1])]; }
+		public: constexpr auto
+		GetCol(size_t i) const
+		{ return Slice<std::remove_reference_t<decltype(*this)>, true>(*this, i); }
 		
-		public: constexpr auto& operator[](std::initializer_list<size_t> col_row) const
-		{ return M_data[Index_Absolute(col_row.begin()[0],col_row.begin()[1])]; }
+		public: constexpr auto
+		GetRow(size_t i)
+		{ return Slice<std::remove_reference_t<decltype(*this)>, false>(*this, i); }
+		
+		public: constexpr auto
+		GetRow(size_t i) const
+		{ return Slice<std::remove_reference_t<decltype(*this)>, false>(*this, i); }
+		
+		public: constexpr auto&
+		operator[](std::initializer_list<size_t> col_row)
+		{ return M_data[IndexAbs( col_row.begin()[0] , col_row.begin()[1] )]; }
+		
+		public: constexpr auto&
+		operator[](std::initializer_list<size_t> col_row) const
+		{ return M_data[IndexAbs( col_row.begin()[0] , col_row.begin()[1] )]; }
+		
+		public: constexpr auto
+		operator[](size_t i)
+		{ return GetCol(i); }
+		
+		public: constexpr auto
+		operator[](size_t i) const
+		{ return GetCol(i); }
 		
 		private: T
 		M_data[SIZE];
