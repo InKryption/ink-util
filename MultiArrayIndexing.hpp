6b2d82f5e4b7ec86c7444d0b9c98f57b07e7ceba
InK <InKryption07@gmail.com>
2021-03-23 11:49:54 +0100

    Documentation

--- MultiArrayIndexing.hpp
+++ MultiArrayIndexing.hpp
@@ -18,6 +18,9 @@ namespace ink {
 	 * @brief This functions generates and returns a (constexpr) lambda based off of the specified dimensions,
 	 * which itself is a function which generates an absolute index from multiple dimensional indexes.
 	 * 
+	 * i.e. take a 2D array | int arr2D[2][3] |, and map it to a 1D array | int arr1D[6] |.
+	 * The mapping using this function would be like of the form | auto lambda = f(3,2); arr2D[1,2] == arr1D[lambda(1,2)] |.
+	 * 
 	 * @param dimension_sizes
 	 * These parameters (this parameter pack) represent the dimensions of, e.g., an array (albeit in reverse order).
 	 * So the correspondence would be of the form | int[HEIGHT,WIDTH] | f(WIDTH,HEIGHT) |.
@@ -25,9 +28,6 @@ namespace ink {
 	 * @return The returned lambda takes the same number of arguments as dimension_sizes supplied to the generator function.
 	 * Each index argument represents an index into the dimension of the same position, and as such is expected to
 	 * be less than the size of said dimension (index is out-of-bounds otherwise).
-	 * 
-	 * i.e. take a 2D array | int arr2D[2][3] |, and map it to a 1D array | int arr1D[6] |.
-	 * The mapping using this function would be like of the form | auto lambda = f(3,2); arr2D[1,2] == arr1D[lambda(1,2)] |.
 	 * 
 	 */
 	template<typename... i> requires(std::convertible_to<i,size_t>&&...) static constexpr auto
